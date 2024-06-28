853c619ce95595708a06ebacbc441604c0fbea38
osama4713 <osamatamer471@gmail.com>
2024-06-28 23:11:37 +0300

    upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/My_Portfoilo-Node/", 
+  base: "/My_Portfoilo_Node.js/", 
   plugins: [react()],
 })
